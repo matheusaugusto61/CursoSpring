1db74670079314cffe2f075451081dae6a00b1e0
matheusaugusto61 <matheussouza.1802@aluno.unibh.br>
2021-11-28 18:01:54 -0300

    ajustes no endpoint /categorias{id}

--- src/main/java/com/example/testrest/domain/Produto.java
+++ src/main/java/com/example/testrest/domain/Produto.java
@@ -1,5 +1,7 @@
 package com.example.testrest.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -14,6 +16,7 @@ public class Produto implements Serializable {
     private String nome;
     private Double preco;
 
+    @JsonBackReference
     @ManyToMany
     @JoinTable(name = "PRODUTO_CATEGORIA",
         joinColumns = @JoinColumn(name = "produto_id"),
--- src/main/java/com/example/testrest/services/CategoriaService.java
+++ src/main/java/com/example/testrest/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.example.testrest.services;
 
 import com.example.testrest.domain.Categoria;
 import com.example.testrest.repositories.CategoriaRepository;
+import com.example.testrest.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
