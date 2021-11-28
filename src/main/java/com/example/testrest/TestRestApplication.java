645fdabd91e9f7d68b2dcde3c4ba533f5530a9c4
matheusaugusto61 <matheussouza.1802@aluno.unibh.br>
2021-11-28 16:47:35 -0300

    criando operação de instanciação

--- src/main/java/com/example/testrest/TestRestApplication.java
+++ src/main/java/com/example/testrest/TestRestApplication.java
@@ -1,13 +1,28 @@
 package com.example.testrest;
 
+import com.example.testrest.domain.Categoria;
+import com.example.testrest.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class TestRestApplication {
+public class TestRestApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(TestRestApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Escritório");
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
